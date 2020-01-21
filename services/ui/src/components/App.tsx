ffd26e9ef94fd3675a9186c72594c74d7e636ac1
Andrew Ding <ading25@gmail.com>
2020-01-20 21:47:42 -0800

    plaid UI foundation

---
+++ services/ui/src/components/App.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import axios, { AxiosResponse } from 'axios'
+import PlaidLink, { PlaidLinkProps } from 'react-plaid-link'
+
+class App extends React.Component {
+    // send token to client server
+    handleOnSuccess(
+        token: Parameters<PlaidLinkProps['onSuccess']>[0],
+        metadata: Parameters<PlaidLinkProps['onSuccess']>[1]):
+        ReturnType<PlaidLinkProps['onSuccess']>
+    {
+        axios.post('/api/public_token_exchange', {
+            token,
+        }).then((response: AxiosResponse) => {
+            console.log(response);
+        });
+    }
+    handleOnExit() {
+        // handle the case when your user exits Link
+    }
+    render() {
+        return (
+            <PlaidLink
+                clientName="Your app name"
+                env="sandbox"
+                product={["auth", "transactions"]}
+                publicKey="04bdd52d45ea4c918bfd11712fbcc1"
+                onExit={this.handleOnExit}
+                onSuccess={this.handleOnSuccess}
+            >
+                Open Link and connect your bank!
+            </PlaidLink>
+        )
+    }
+}
+export default App
--- services/ui/src/components/Hello.tsx
+++ services/ui/src/components/Hello.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import App from './App';
 
 export interface HelloProps { compiler: string; framework: string; }
 
 export const Hello = (props: HelloProps) => {
     return (
-        <h1>Hello from {props.compiler} and {props.framework}!</h1>
+        <div>
+            <h1>Hello from {props.compiler} and {props.framework}!</h1>
+            <App />
+        </div>
     );
 }
