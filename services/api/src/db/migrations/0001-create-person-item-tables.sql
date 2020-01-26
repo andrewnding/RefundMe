0c0991edd314e5d0ee7601c9dca4db788c547d95
Andrew Ding <ading25@gmail.com>
2020-01-26 14:29:32 -0800

    create person and item tables

---
+++ services/api/src/db/migrations/0001-create-person-item-tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS person (
+    id UUID DEFAULT uuid_generate_v1() NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS item (
+    item_id VARCHAR NOT NULL,
+    access_token VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+CREATE TABLE IF NOT EXISTS person_item (
+    person_id UUID NOT NULL,
+    item_id VARCHAR NOT NULL,
+    PRIMARY KEY (person_id, item_id)
+);
--- services/api/src/db/migrations/0001-create-users-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id uuid DEFAULT uuid_generate_v1() NOT NULL,
-    email VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
-    PRIMARY KEY (id)
-)
